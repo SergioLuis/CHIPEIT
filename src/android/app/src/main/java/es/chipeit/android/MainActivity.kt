f04f0cd0310c0a3e419039517b0d891cb88a5844
Sergio Luis Para <sergio@sergioluis.es>
2018-03-14 19:10:25 +0000

    Removed chipeitlib dependence from Android project.
    Now, both projects can be again compiled on their own both from command line and from the IDE (IntelliJ / Android Studio).

--- src/android/app/src/main/java/es/chipeit/android/MainActivity.kt
+++ src/android/app/src/main/java/es/chipeit/android/MainActivity.kt
@@ -3,15 +3,10 @@ package es.chipeit.android
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-import es.chipeit.lib.MyClass
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val myObject = MyClass()
-        myObject.sayHelloTo("Sergio")
     }
 }
