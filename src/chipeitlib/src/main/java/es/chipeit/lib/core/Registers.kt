c9f16a36d16ed76d8defd53829d2768cb2260aa8
Sergio Luis Para <sergio@sergioluis.es>
2018-03-21 22:49:11 +0000

    Define the IRegisters interface and the Registers class.

--- src/chipeitlib/src/main/java/es/chipeit/lib/core/Registers.kt
+++ src/chipeitlib/src/main/java/es/chipeit/lib/core/Registers.kt
@@ -1,1 +1,45 @@
 package es.chipeit.lib.core
+
+import es.chipeit.lib.interfaces.IMemory
+import es.chipeit.lib.interfaces.IRegisters
+
+class Registers(override val vReg: IMemory<Byte>) : IRegisters {
+    private val regI: ByteArray = ByteArray(2)
+    private val regPC : ByteArray = ByteArray(2)
+
+    private var regDT: Byte = 0x0
+    private var regST: Byte = 0x0
+    private var regSC: Byte = 0x0
+
+    override fun getI(): ByteArray {
+        return regI
+    }
+
+    override fun getDT(): Byte {
+        return regDT
+    }
+
+    override fun setDT(value: Byte) {
+        regDT = value
+    }
+
+    override fun getST(): Byte {
+        return regST
+    }
+
+    override fun setST(value: Byte) {
+        regSC = value
+    }
+
+    override fun getPC(): ByteArray {
+        return regPC
+    }
+
+    override fun getSC(): Byte {
+        return regSC
+    }
+
+    override fun setSC(value: Byte) {
+        regSC = value
+    }
+}
--- src/chipeitlib/src/main/java/es/chipeit/lib/interfaces/IRegisters.kt
+++ src/chipeitlib/src/main/java/es/chipeit/lib/interfaces/IRegisters.kt
@@ -1,1 +1,18 @@
 package es.chipeit.lib.interfaces
+
+interface IRegisters {
+    val vReg : IMemory<Byte>
+
+    fun getI(): ByteArray
+
+    fun getDT(): Byte
+    fun setDT(value: Byte)
+
+    fun getST(): Byte
+    fun setST(value: Byte)
+
+    fun getPC(): ByteArray
+
+    fun getSC(): Byte
+    fun setSC(value: Byte)
+}
