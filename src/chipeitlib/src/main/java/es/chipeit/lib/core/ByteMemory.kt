5ec446e06e828b18d672e67176d63454fcc4d0c7
Sergio Luis Para <sergio@sergioluis.es>
2018-03-21 22:55:12 +0000

    Implement the ByteMemory and the ByteArrayMemory classes.
    The first one is intended for single-byte memory, such as the main memory. The second one is intended for double-byte memory, such as the stack.

--- src/chipeitlib/src/main/java/es/chipeit/lib/core/ByteMemory.kt
+++ src/chipeitlib/src/main/java/es/chipeit/lib/core/ByteMemory.kt
@@ -1,1 +1,17 @@
 package es.chipeit.lib.core
+
+import es.chipeit.lib.interfaces.IMemory
+
+internal class ByteMemory(size: Int): IMemory<Byte> {
+    private var memory: ByteArray = ByteArray(size)
+
+    override fun get(index: Int): Byte {
+        // TODO add log
+        return memory[index]
+    }
+
+    override fun set(index: Int, value: Byte) {
+        // TODO add log
+        memory[index] = value
+    }
+}
