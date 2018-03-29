a8d0d49a93e646e609c0cf2f0e81802fc7be7cf4
Sergio Luis Para <sergio@sergioluis.es>
2018-03-29 23:36:20 +0000

    Start implementing the Chipeit class
    It implements the proposed mechanism for triggering both the Cpu and
    the Timers clock dividers. The rest of the code is pretty
    self-explanatory.

--- src/chipeitlib/src/main/java/es/chipeit/lib/emulator/Chipeit.kt
+++ src/chipeitlib/src/main/java/es/chipeit/lib/emulator/Chipeit.kt
@@ -1,51 +1,72 @@
 package es.chipeit.lib.emulator
 
-class Chipeit {
+import kotlin.math.max
+import kotlin.math.min
+
+import es.chipeit.lib.core.*
+import es.chipeit.lib.core.log.LoggedMemory
+import es.chipeit.lib.interfaces.hzToMs
+import es.chipeit.lib.io.IRenderer
+import es.chipeit.lib.io.ISwitchObserver
+
+class Chipeit(
+        renderer: IRenderer,
+        soundPlayer: ISwitchObserver,
+        romContent: ByteArray,
+        cpuClockRate: Short = 500,
+        timersClockRate: Short = 60,
+        private val sleeper: ISleeper
+) {
     @Volatile
     private var running: Boolean = false
+    private val memory = LoggedMemory(
+            "Main memory",
+            PaddedMemory(ByteMemory(romContent), 200)
+    )
+    private val registers = Registers(LoggedMemory(
+            "Registers memory",
+            ByteMemory(ByteArray(16)))
+    )
+    private val stack = LoggedMemory(
+            "Stack memory",
+            ShortMemory(ShortArray(16))
+    )
+
+    private val cpu = Cpu(memory, registers, stack, renderer)
+    private val soundTimer = Timer(soundPlayer)
+    private val eventTimer = Timer() // FIXME: I still don't know the f*cking purpose of this
+
+    private val clock = Clock()
+
+    private val cpuClockDivider = ClockDivider(clock, hzToMs(cpuClockRate))
+    private val timersClockDivider = ClockDivider(clock, hzToMs(timersClockRate))
+
+    init {
+        cpuClockDivider.observers.add(cpu)
+        timersClockDivider.observers.add(soundTimer)
+        timersClockDivider.observers.add(eventTimer)
+    }
 
     fun run() {
         running = true
+        cpuClockDivider.init()
+        timersClockDivider.init()
 
         while (running) {
-            // TODO: Trigger clock dividers.
-            // TODO: Sleep (seriously).
+            cpuClockDivider.trigger()
+            timersClockDivider.trigger()
+
+            val timeToSleep =
+                max(0, min(
+                    cpuClockDivider.getMsLeft(),
+                    timersClockDivider.getMsLeft())
+                )
+
+            sleeper.sleep(timeToSleep)
         }
     }
 
     fun stop() {
-        running = false;
+        running = false
     }
 }
-
-/*
-
-TODO: Check if this way of reasoning works! Consider coroutines
-
-Clock Thread {
-
-    // Thread actual work
-    fun doWork() {
-        while (true) {
-            // This IClockDivider handles countdown timers
-            delaysClockDivider.trigger()
-
-            // This IClockDivider triggers CPU cycle execution
-            instructionsClockDivider.trigger()
-
-            Thread.Sleep(
-                Math.max(0,
-                    Math.min(
-                        delaysClockDivider.msLeft(),
-                        instructionsClockDivider.msLeft()
-                    )
-                )
-            )
-        }
-    }
-
-    IClockDivider delaysClockDivider          // 60Hz
-    IClockDivider instructionsClockDivider    // 500Hz
-}
-
- */
