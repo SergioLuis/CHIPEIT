b2cba2afcef322bb487f3ba5ff26c648214b90fe
Sergio Luis Para <sergio@sergioluis.es>
2018-04-04 10:02:50 +0000

    Converted the ShortMemory into a IntMemory
    We have here the same problem as with the Program Counter. In order to
    avoid endless conversions in runtime, we have to use a IMemory<Int> to
    back the Stack.

--- src/chipeitlib/src/main/java/es/chipeit/lib/core/IntMemory.kt
+++ src/chipeitlib/src/main/java/es/chipeit/lib/core/IntMemory.kt
@@ -2,19 +2,19 @@ package es.chipeit.lib.core
 
 import es.chipeit.lib.interfaces.IMemory
 
-internal class ShortMemory(private val memory: ShortArray): IMemory<Short> {
+internal class IntMemory(private val memory: IntArray): IMemory<Int> {
     override val size: Int
         get() = memory.size
 
-    override fun get(index: Int): Short {
+    override fun get(index: Int): Int {
         return memory[index]
     }
 
-    override fun set(index: Int, value: Short) {
+    override fun set(index: Int, value: Int) {
         memory[index] = value
     }
 
-    override fun fill(element: Short, fromIndex: Int, toIndex: Int) {
+    override fun fill(element: Int, fromIndex: Int, toIndex: Int) {
         memory.fill(element, fromIndex, toIndex)
     }
 }
