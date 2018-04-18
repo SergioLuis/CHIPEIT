1ad0ff38bae7cdebdc2c994608c15022ba2b27e9
Sergio Luis Para <sergio@sergioluis.es>
2018-04-18 18:28:34 +0000

    Add the 'ICoreKeyboard' interface
    In an analogous way to the 'IUserKeyboard', this interface only contains
    methods useful to the core of the library, like the methods that
    controls the latching to the next key release.
    
    Also, added the soundTimer as a member of the keyboard, as it needs to
    directly change its register value based on whether the keyboard is
    waiting for a key to be released or not.

--- src/chipeitlib/src/main/java/es/chipeit/lib/core/Keyboard.kt
+++ src/chipeitlib/src/main/java/es/chipeit/lib/core/Keyboard.kt
@@ -1,35 +1,46 @@
 package es.chipeit.lib.core
 
-import es.chipeit.lib.io.IKeyboard
+import es.chipeit.lib.interfaces.ICoreKeyboard
+import es.chipeit.lib.interfaces.ITimer
+import es.chipeit.lib.io.IUserKeyboard
 
-class Keyboard : IKeyboard {
+internal class Keyboard(override val soundTimer: ITimer) : IUserKeyboard, ICoreKeyboard {
     private var pressedKeys: Int = 0
 
-    private var _capturedKeyRelease: IKeyboard.Keys = IKeyboard.Keys.NONE
-    internal val capturedKeyRelease: IKeyboard.Keys
+    private var _capturedKeyRelease: IUserKeyboard.Keys = IUserKeyboard.Keys.NONE
+    override val capturedKeyRelease: IUserKeyboard.Keys
         get() = _capturedKeyRelease
 
     private var _isCapturingNextKeyRelease: Boolean = false
-    internal val isCapturingNextKeyRelease: Boolean
+    override val isCapturingNextKeyRelease: Boolean
         get() = _isCapturingNextKeyRelease
 
-    internal fun captureNextKeyRelease() {
+    override fun captureNextKeyRelease() {
         _isCapturingNextKeyRelease = true
+
+        if (pressedKeys != 0)
+            soundTimer.setRegister(4)
     }
 
-    internal fun clearLastCapturedKeyRelease() {
-        _capturedKeyRelease = IKeyboard.Keys.NONE
+    override fun clearLastCapturedKeyRelease() {
+        _capturedKeyRelease = IUserKeyboard.Keys.NONE
     }
 
-    internal fun isPressed(key: IKeyboard.Keys): Boolean {
+    override fun updateSoundTimerCounterIfNeeded() {
+        if (isCapturingNextKeyRelease && pressedKeys != 0)
+            soundTimer.setRegister(4)
+    }
+
+    override fun isPressed(key: IUserKeyboard.Keys): Boolean {
         return (pressedKeys and key.data.flags) != 0
     }
 
-    override fun pressKey(key: IKeyboard.Keys) {
+    override fun pressKey(key: IUserKeyboard.Keys) {
         pressedKeys = pressedKeys or key.data.flags
+        soundTimer.setRegister(4)
     }
 
-    override fun releaseKey(key: IKeyboard.Keys) {
+    override fun releaseKey(key: IUserKeyboard.Keys) {
         if (_isCapturingNextKeyRelease && isPressed(key))
         {
             _capturedKeyRelease = key
