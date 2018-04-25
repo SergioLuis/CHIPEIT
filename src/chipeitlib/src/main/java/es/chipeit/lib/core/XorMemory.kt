e9462d0c6c6146899854d775e6a98b33934842e7
Mikel Cazorla Pérez <mikelcaz@ucm.es>
2018-04-25 22:04:23 +0200

    Add XorMemory class and tests
    XorMemory will isolate the xor operation needed for the graphic memory.

---
+++ src/chipeitlib/src/main/java/es/chipeit/lib/core/XorMemory.kt
@@ -0,0 +1,17 @@
+package es.chipeit.lib.core
+
+import es.chipeit.lib.interfaces.IMemory
+import kotlin.experimental.xor
+
+internal class XorMemory(private val memory: IMemory<Byte>) : IMemory<Byte> {
+    override val size: Int
+        get() = memory.size
+
+    override fun get(index: Int): Byte {
+        return memory[index]
+    }
+
+    override fun set(index: Int, value: Byte) {
+        memory[index] = memory[index] xor value
+    }
+}
---
+++ src/chipeitlib/src/test/java/es/chipeit/lib/core/XorMemoryTests.kt
@@ -0,0 +1,82 @@
+package es.chipeit.lib.core
+
+import es.chipeit.lib.interfaces.IMemory
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+class XorMemoryTests {
+    internal fun switchTestFrom(memory: IMemory<Byte>, index: Int, with: Byte, to: Byte): Byte {
+        val from: Byte = memory[index]
+
+        assertNotEquals(from, to)
+
+        memory[index] = with
+        assertEquals(to, memory[index])
+
+        memory[index] = with
+        assertEquals(from, memory[index])
+
+        return from
+    }
+
+    internal fun noSwitchTestFrom(memory: IMemory<Byte>, index: Int, with: Byte): Byte {
+        val from: Byte = memory[index]
+
+        memory[index] = with
+        assertEquals(from, memory[index])
+
+        return from
+    }
+
+    @Test
+    fun xorTest() {
+        val ones: Byte = 0xFF.toByte()
+
+        var memory = ByteMemory(ByteArray(6))
+        var xorMemory = XorMemory(memory)
+
+        for (index in 0..memory.size - 1)
+            assertEquals(0x00, memory[index])
+
+        // No switch from zeros with zeros
+        assertEquals(
+                0x00,
+                noSwitchTestFrom(xorMemory, 0, 0x00)
+        )
+
+        // Switch from zeros with ones to ones
+        assertEquals(
+                0x00,
+                switchTestFrom(xorMemory, 1, ones, ones)
+        )
+
+        // No switch from ones with zeros
+        memory[2] = ones
+        assertEquals(
+                ones,
+                noSwitchTestFrom(xorMemory, 2, 0x00)
+        )
+
+        // Switch from ones with ones to zeros
+        memory[3] = ones
+        assertEquals(
+                ones,
+                switchTestFrom(xorMemory, 3, ones, 0x00)
+        )
+
+        // Nibble switch
+        memory[4] = 0x0F
+        assertEquals(
+                0x0F,
+                switchTestFrom(xorMemory, 4, 0xFF.toByte(), 0xF0.toByte())
+        )
+
+        // Half nibble switch
+        memory[5] = 0x3C
+        assertEquals(
+                0x3C,
+                switchTestFrom(xorMemory, 5, 0xFF.toByte(), 0xC3.toByte())
+        )
+    }
+}
