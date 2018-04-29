41e247f632007cd3c81e30057f7474ef5d31650d
Mikel Cazorla Pérez <mikelcaz@ucm.es>
2018-04-29 15:33:18 +0200

    Update drwVxVyNibble instruction

--- src/chipeitlib/src/main/java/es/chipeit/lib/core/InstructionSet.kt
+++ src/chipeitlib/src/main/java/es/chipeit/lib/core/InstructionSet.kt
@@ -135,7 +135,7 @@ internal fun drwVxVyNibble(
     for (i in 0 until height) {
         val spriteRow = memory[registers.i + i]
 
-        pixelCleared = graphicMemory.drawLine(x, y + i, spriteRow)
+        pixelCleared = graphicMemory.drawRow(x, y + i, spriteRow)
     }
 
     registers.pc += 2
