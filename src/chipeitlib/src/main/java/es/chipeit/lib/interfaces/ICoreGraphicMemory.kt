4b3f7a1cf45db1dec29de2fcc2570394e3a78871
Mikel Cazorla Pérez <mikelcaz@ucm.es>
2018-04-29 15:31:53 +0200

    Polish ICoreGraphicMemory interface

--- src/chipeitlib/src/main/java/es/chipeit/lib/interfaces/ICoreGraphicMemory.kt
+++ src/chipeitlib/src/main/java/es/chipeit/lib/interfaces/ICoreGraphicMemory.kt
@@ -3,6 +3,6 @@ package es.chipeit.lib.interfaces
 import es.chipeit.lib.io.IGraphicMemory
 
 internal interface ICoreGraphicMemory : IGraphicMemory {
-    fun drawLine(x: Int, y: Int, value: Byte): Boolean
+    fun drawRow(x: Int, y: Int, bitline: Byte): Boolean
     fun clear()
 }
